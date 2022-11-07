37e48e3733f8727b4186de55dd02dac7d0c6a408
Nusendra Hanggarawan <admin@nusendra.com>
2022-11-07 21:42:53 +0700

    feat: add command line

--- src/schema-generator.ts
+++ src/schema-generator.ts
@@ -2,25 +2,29 @@ import path from "path";
 import * as tjs from "typescript-json-schema";
 import fs from "fs";
 
-const sourceTsSchema = process.argv[2];
-const generatedFileName = process.argv[3];
+const run = () => {
+	const sourceTsSchema = process.argv[2];
+	const generatedFileName = process.argv[3];
 
-const program = tjs.getProgramFromFiles(
-	[path.resolve(sourceTsSchema)],
-	{
-		strictNullChecks: true,
-	},
-	"./"
-);
+	const program = tjs.getProgramFromFiles(
+		[path.resolve(sourceTsSchema)],
+		{
+			strictNullChecks: true,
+		},
+		"./"
+	);
 
-const schema = tjs.generateSchema(program, "*", {
-	required: true,
-	ref: false,
-});
+	const schema = tjs.generateSchema(program, "*", {
+		required: true,
+		ref: false,
+	});
 
-fs.writeFileSync(
-	`${generatedFileName}.ts`,
-	`const schema = ` +
-		JSON.stringify(schema) +
-		` as const;\n\nexport default schema.definitions;`
-);
+	fs.writeFileSync(
+		`${generatedFileName}.ts`,
+		`const schema = ` +
+			JSON.stringify(schema) +
+			` as const;\n\nexport default schema.definitions;`
+	);
+};
+
+export default run;
